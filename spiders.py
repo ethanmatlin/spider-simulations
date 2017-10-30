de7eda6477e1c28a84da4df82bcccab40c06290e
ethanmatlin <ethanmatlin@gmail.com>
2017-10-30 13:15:23 -0700

    added new features

--- spiders.py
+++ spiders.py
@@ -7,17 +7,26 @@ numSpiders = 5
 numLocations = 5
 xi = 1 
 kappa = 1
+T = 100
 
 # Randomly assign all 5 spiders a position
 spiderLocs = [(random.randint(0,numLocations-1), random.randint(0,numLocations-1)) for i in range(numSpiders)]
-s = np.ones(5)
+#An array of arrays: each of the J spiders needs room for T different fitnesses over their lifetime.
+F = [np.ones(T) for i in range(numSpiders)]
+
 u = [[np.random.poisson(1) for i in range(numLocations)] for k in range(numLocations)]
+
+def size(F, j, t):
+	sum = 0 
+	for tau in range(t):
+		sum = sum + ((4*(tau-t/2)**2)/t**2)*F[j][tau]
+	return sum 
 
 def dist(old_loc, new_loc):
 	return 1
 
-def fitness_func(newLoc_x, newLoc_y, spider):
-	return s[spider]*((u[newLoc_x][newLoc_y]/v(newLoc_x,newLoc_y))+v(newLoc_x,newLoc_y)) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
+def fitness_func(newLoc_x, newLoc_y, spider, F, t):
+	return size(F, spider, t)*((u[newLoc_x][newLoc_y]/v(newLoc_x,newLoc_y))+v(newLoc_x,newLoc_y)) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
 
 def v(loc_x,loc_y):
 	counter = 0 
@@ -29,10 +38,10 @@ def v(loc_x,loc_y):
 	return counter
 
 
-for t in range(100):
+for t in range(T):
 	for j in range(numSpiders):
 		# Spiders eat
 		#s(j) = 
 		# Spiders calculate fitness from other locations
-		F = [[fitness_func(i, k, j) for i in range(numLocations)] for k in range(numLocations)]
-		print(F)
+		F_calc = [[fitness_func(i, k, j, F, t) for i in range(numLocations)] for k in range(numLocations)]
+		print(np.argmin(F_calc))
