3d686911ccc5310a5284c785be0a17ed79101aee
ethanmatlin <ethanmatlin@gmail.com>
2017-11-03 16:53:22 -0700

    added new features

--- spiders.py
+++ spiders.py
@@ -11,6 +11,7 @@ T = 100
 
 # Randomly assign all 5 spiders a position
 spiderLocs = [(random.randint(0,numLocations-1), random.randint(0,numLocations-1)) for i in range(numSpiders)]
+print(spiderLocs)
 #An array of arrays: each of the J spiders needs room for T different fitnesses over their lifetime.
 F = [np.ones(T) for i in range(numSpiders)]
 
@@ -25,7 +26,9 @@ def size(F, j, t):
 def dist(old_loc, new_loc):
 	return 1
 
-def fitness_func(newLoc_x, newLoc_y, spider, F, t):
+def fitness_func(newLoc, spider, F, t):
+	newLoc_x = newLoc[0] 
+	newLoc_y = newLoc[1]
 	return size(F, spider, t)*((u[newLoc_x][newLoc_y]/v(newLoc_x,newLoc_y))+v(newLoc_x,newLoc_y)) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
 
 def v(loc_x,loc_y):
@@ -41,7 +44,18 @@ def v(loc_x,loc_y):
 for t in range(T):
 	for j in range(numSpiders):
 		# Spiders eat
-		#s(j) = 
+		F[j][t] = fitness_func(spiderLocs[j], j, F, t) 
 		# Spiders calculate fitness from other locations
-		F_calc = [[fitness_func(i, k, j, F, t) for i in range(numLocations)] for k in range(numLocations)]
-		print(np.argmin(F_calc))
+		F_calc = [[fitness_func((i, k), j, F, t) for i in range(numLocations)] for k in range(numLocations)]
+		#print(F_calc)
+		#Gives index number of a flattened matrix
+		#randomize this in case there's a tie!
+	print(np.argmax(F_calc))
+
+
+#ways to test. what is answer should be
+# pcolor in matlab
+#-if all prey is at one spot, spiders should all go there
+#-etc.
+# plot fitness function over time and see if agrees with what should be happening
+# plot fitness function over space 
