de531ce2f68485912f104ad0b1b8bd8e1556559a
ethanmatlin <ethanmatlin@gmail.com>
2017-11-09 15:29:55 -0800

    counter=1

--- spiders.py
+++ spiders.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 
 
+
 numSpiders = 5
 # Actually number of locations is numLocations^2 since we're on a 2x2 grid.
 numLocations = 5
@@ -19,7 +20,7 @@ F = [np.ones(T) for i in range(numSpiders)]
 
 
 def size(F, j, t):
-	sum = 1 
+	sum = 1
 	for tau in range(t):
 		sum = sum + ((4*(tau-t/2)**2)/t**2)*F[j][tau]
 	return sum 
@@ -36,14 +37,17 @@ def fitness_func(newLoc, spider, F, t):
 	return size(F, spider, t)*((u[newLoc_x][newLoc_y]/v(newLoc_x,newLoc_y))+v(newLoc_x,newLoc_y)) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
 
 def v(loc_x,loc_y):
-	counter = 0 
+	counter = 1 
 	for i in range(len(spiderLocs)):
 		if (spiderLocs[i][0]==loc_x & spiderLocs[i][1]==loc_y):
 			counter = counter + 1
-	if (counter==0):
-		counter=0.0001
 	return counter
 
+def rargmax(vector):
+    """ Argmax that chooses randomly among eligible maximum indices. """
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0]
+    return random.choice(indices)
 
 for t in range(T):
 	u = [[np.random.poisson(2) for i in range(numLocations)] for k in range(numLocations)]
@@ -55,10 +59,13 @@ for t in range(T):
 		F_calc = [[fitness_func((i, k), j, F, t) for i in range(numLocations)] for k in range(numLocations)]
 		#print(F_calc)
 		#aaa
-		#Gives index number of a flattened matrix
 		#randomize this in case there's a tie!
 		F_calc = np.array(F_calc)
+		#print(F_calc)
 		i,j = np.unravel_index(np.argmax(F_calc), F_calc.shape)
+		#i,j = np.unravel_index(rargmax(F_calc), F_calc.shape)
+		#print(i)
+		#print(j)
 		spiderLocs[j] = (i,j)
 	print(spiderLocs)
 	
