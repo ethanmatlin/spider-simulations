18ed9f1424e9266f6f4566d2460780e7a7dd5640
ethanmatlin <ethanmatlin@gmail.com>
2017-11-16 20:32:27 -0800

    spider_plotting plots and fixes many bugs from before

--- spider_plotting.py
+++ spider_plotting.py
@@ -12,10 +12,18 @@ plotly.tools.set_credentials_file(username='ethanmatlin', api_key='3eIOZHrSqA7Lo
 numSpiders = 100
 # Actually number of locations is numLocations^2 since we're on a 2x2 grid.
 numLocations = 5
-xi = 1 
-kappa = 1
-T = 100
+xi = .1 
+kappa = .1
+T = 50
 numMales=numSpiders
+
+def numAtLoc(loc_y,loc_x):
+	counter = 0
+	#print(spiderLocs)
+	for i in range(len(spiderLocs)):
+		if (spiderLocs[i][0]==loc_y and spiderLocs[i][1]==loc_x):
+			counter = counter + 1
+	return counter
 
 # Randomly assign all 5 spiders a position
 spiderLocs = [(random.randint(0,numLocations-1), random.randint(0,numLocations-1)) for i in range(numSpiders)]
@@ -25,29 +33,38 @@ maleLocs = [(random.randint(0,numLocations-1), random.randint(0,numLocations-1))
 #An array of arrays: each of the J spiders needs room for T different fitnesses over their lifetime.
 F = [np.ones(T) for i in range(numSpiders)]
 
+start_matrix = np.ones((numLocations, numLocations))
+for k in range(numLocations):
+	for l in range(numLocations):
+		start_matrix[k, l] = numAtLoc(k,l)
+
+print(start_matrix)
 
 
 def size(F, j, t):
 	sum = 1
 	for tau in range(t):
-		sum = sum + ((4*(tau-t/2)**2)/t**2)*F[j][tau]
+		sum = sum + (3/t)*((4*(tau-t/2)**2)/t**2)*F[j][tau]
 	return sum 
 
 def dist(old_loc, new_loc):
 	return 1
 
 def fitness_func(newLoc, spider, F, t):
-	newLoc_x = newLoc[0] 
-	newLoc_y = newLoc[1]
-	#print(newLoc_y)
-	#print(newLoc_x)
-	#aaa
-	return size(F, spider, t)/v(newLoc_x,newLoc_y, t)*((u[newLoc_x][newLoc_y])) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
+	if (spiderLocs[spider] == (-99,-99)):
+		return 0
+	else:
+		newLoc_x = newLoc[0] 
+		newLoc_y = newLoc[1]
+		#print(newLoc_y)
+		#print(newLoc_x)
+		#aaa
+		return size(F, spider, t)/v(newLoc_x,newLoc_y, t)*((u[newLoc_x][newLoc_y])) - xi*dist(spiderLocs[spider],(newLoc_x,newLoc_y)) - kappa
 
 def v(loc_x,loc_y, t):
 	mass = 1 
 	for i in range(numSpiders):
-		if (spiderLocs[i][0]==loc_x & spiderLocs[i][1]==loc_y):
+		if (spiderLocs[i][0]==loc_x and spiderLocs[i][1]==loc_y):
 			mass = mass + F[i][t]
 	return mass
 
@@ -57,50 +74,52 @@ def rargmax(vector):
     indices = np.nonzero(vector == m)[0]
     return random.choice(indices)
 
-def numAtLoc(loc_x,loc_y):
-	counter = 0
-	for i in range(len(spiderLocs)):
-		if (spiderLocs[i][0]==loc_x & spiderLocs[i][1]==loc_y):
-			counter = counter + 1
-	return counter
+
 
 allMats = [np.ones((numLocations,numLocations)) for i in range(100)]
-print(allMats)
+#print(allMats)
 
 for t in range(T):
-	u = [[np.random.poisson(2) for i in range(numLocations)] for k in range(numLocations)]
-	#print(u)
+	u = [[np.random.poisson(5) for i in range(numLocations)] for k in range(numLocations)]
+	#u = [[0 for i in range(numLocations)] for k in range(numLocations)]
+	#u[1][1] = 100
 	for j in range(numSpiders):
 		# Spiders eat
 		F[j][t] = fitness_func(spiderLocs[j], j, F, t) 
-		# Spiders calculate fitness from other locations
-		F_calc = [[fitness_func((i, k), j, F, t) for i in range(numLocations)] for k in range(numLocations)]
-		#print(F_calc)
-		#aaa
-		#randomize this in case there's a tie!
-		F_calc = np.array(F_calc)
-		#print(F_calc)
-		i,j = np.unravel_index(np.argmax(F_calc), F_calc.shape)
-		#i,j = np.unravel_index(rargmax(F_calc), F_calc.shape)
-		#print(i)
-		#print(j)
-		spiderLocs[j] = (i,j)
-	print(spiderLocs)
+		if (F[j][t]<0):
+			spiderLocs[j]=(-99,-99)
+		else:
+			# Spiders calculate fitness from other locations
+			F_calc = [[fitness_func((i, k), j, F, t) for i in range(numLocations)] for k in range(numLocations)]
+			#print(F_calc)
+			#aaa
+			#randomize this in case there's a tie!
+			F_calc = np.array(F_calc)
+		
+			#i,j = np.unravel_index(np.argmax(F_calc), F_calc.shape)
+			i,j = np.unravel_index(rargmax(F_calc), F_calc.shape)
+			#print(i)
+			#print(j)
+			spiderLocs[j] = (i,j)
+	print(F_calc)
+	#print(spiderLocs)
 	
 	matrix = np.ones((numLocations, numLocations))
 	for k in range(numLocations):
 		for l in range(numLocations):
 			matrix[k, l] = numAtLoc(k,l)
-	print(matrix)
+	#print(matrix)
 	allMats[t] = matrix
 	#print(i,j)
-print(allMats)
+allMats[0]=start_matrix
+print(allMats[0])
+print(allMats[49])
 
 trace = go.Heatmap(z=allMats[0])
 data=[trace]
 py.iplot(data, filename='heatmap_1')
 
-trace = go.Heatmap(z=allMats[99])
+trace = go.Heatmap(z=allMats[49])
 data=[trace]
 py.iplot(data, filename='heatmap_99')
 
