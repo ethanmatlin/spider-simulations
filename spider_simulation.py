d5f2b7e28a76576f8f7889537dd5e0b32ff95de5
ethanmatlin <ethanmatlin@gmail.com>
2018-04-10 21:29:29 -0700

    updated code and there's a lot of new figures

--- spider_simulation.py
+++ spider_simulation.py
@@ -8,6 +8,9 @@ import h5py
 import time 
 import matplotlib.pyplot as plt
 import pandas as pd
+import mpl_toolkits.axes_grid1 as axes_grid1
+import math 
+
 
 
 plotly.tools.set_credentials_file(username='ethanmatlin', api_key='3eIOZHrSqA7LoUdFIuGI')
@@ -39,15 +42,22 @@ def number_at_location(row,col, location, num_spiders):
 def size_continuous(F_panel, j, t):
 	""" Computes size of spider j by taking weighted sum of all previous fitnesses from tau=1, ..., t (where t is the current period) """
 	#print(F_panel[j,(t-4):t])
-	sum = 1
-	for tau in range(t):
-		sum = sum + (3/t)*((4*(t/2-tau)**2)/t**2)*F_panel[j,tau]
+	sum = 0
+	for tau in range(1,t):
+		#pringrt(F_panel[j,tau])
+		## CHANGED TO 1/t INSTEAD TO FIX THE HUGE SPIKE PROBLEM	
+		sum = sum + (1/t)*((4*(t/2-tau)**2)/t**2)*F_panel[j,tau]
 	return sum 
 
 def rescale_size(sizes_crosssection, num_spiders):
 	rescaled_sizes = np.zeros(num_spiders)
 	#     Large     Medium       Mini      Small 
 	#	0.05155321 0.34765367 0.10905486 0.49173827 
+	print(sizes_crosssection)
+	print("95" + str(np.percentile(sizes_crosssection,95)))
+	print("65" + str(np.percentile(sizes_crosssection,65)))
+	print("11" + str(np.percentile(sizes_crosssection,11)))
+	print("mean" + str(np.mean(sizes_crosssection)))
 	for j in range(num_spiders):
 		if (sizes_crosssection[j]>np.percentile(sizes_crosssection,95)):
 			rescaled_sizes[j] = 4
@@ -57,6 +67,7 @@ def rescale_size(sizes_crosssection, num_spiders):
 			rescaled_sizes[j] = 2
 		else: 
 			rescaled_sizes[j] = 1
+	#print(np.mean(rescaled_sizes))
 	return rescaled_sizes 
 
  	#if sum(F_panel[j,(t-4):t] > )
@@ -66,7 +77,7 @@ def dist(old_loc, new_loc):
 	"""Computes distance between two locations"""
 	return ((old_loc[0]-new_loc[0])**2+(old_loc[1]-new_loc[1])**2)**.5
 
-def fitness_func(newLoc, spider, F_panel, t, location, locations_of_others, sizes_crosssection, u, num_spiders, xi, kappa):
+def fitness_func(newLoc, spider, t, locations, sizes_crosssection, u, num_spiders, xi, kappa):
 	"""Computes fitness function for spider
 		- newLoc is location to evaluate fitness at
 		- spider is the spider unravel_index
@@ -74,23 +85,24 @@ def fitness_func(newLoc, spider, F_panel, t, location, locations_of_others, size
 		- t is the current time period
 		- location is the spider's current locations_panel
 		- locations_of_others are the locations of all other spiders"""
-	if (location[0]<0):
+	if (locations[spider,t][0]<0):
 		return -20
 	else:
 		# newLoc[0] is the row (first element in tuple)
 		newLoc_row = newLoc[0] 
 		#newLoc[1] is the column (second element in tuple)
 		newLoc_col = newLoc[1]
-		return sizes_crosssection[spider]/v(newLoc_row,newLoc_col, t, spider, 
-			locations_of_others, sizes_crosssection, num_spiders, F_panel)*((u[newLoc_row][newLoc_col])) - xi*dist(location,newLoc) - kappa
+		return 2*sizes_crosssection[spider]/v(newLoc_row,newLoc_col, t, spider, 
+			locations[:,t], sizes_crosssection, num_spiders)*((u[newLoc_row][newLoc_col])) - xi*dist(locations[spider,t],newLoc) - kappa
 		#return size_continuous(F_panel, spider, t)/v(newLoc_row,newLoc_col, t, spider, 
 			#locations_of_others, sizes_crosssection, num_spiders, F_panel)*((u[newLoc_row][newLoc_col])) - xi*dist(location,newLoc) - kappa
 
-def v(row,col, t, k, location, sizes_crosssection, num_spiders, F_panel):
+def v(row,col, t, k, location, sizes_crosssection, num_spiders):
 	"""Computes mass of spiders at a location (the sum of the current fitnesses of all spiders"""
 	mass = 1
 	for j in range(num_spiders):
-		if (j!=k and location[j,0]==row and location[j,1]==col and F_panel[j,t]>=0):
+		if (j!=k and location[j,0]==row and location[j,1]==col):
+			#and F_panel[j,t]>=0
 			#print(sizes_crosssection[j])
 			mass = mass + 1
 	# for j in range(num_spiders):
@@ -125,15 +137,24 @@ def simulate(meta_parameters):
 	random_movement = meta_parameters['random_movement']
 	random_movement_sd = meta_parameters['random_movement_sd']
 	smooth = meta_parameters['smooth']
+	all_at_once = meta_parameters['all_at_once']
+	start_clusters = meta_parameters['start_clusters']
 	numMales=num_spiders
 
 	### INITIALIZE ALL ARRAYS/PANELS
 	# Initialize locations_panel: num_spiders x T matrix in which rows correspond to spiders and columns correspond to time periods.
 	locations_panel = [[(1,1) for t in range(T)] for j in range(num_spiders)]
 	# Randomly assign all spiders a position
-	for j in range(num_spiders):
-		locations_panel[j][0] = (random.randint(0,num_locations-1), random.randint(0,num_locations-1))
-	locations_panel = np.array(locations_panel)
+	if (start_clusters):
+		for j in range(int(num_spiders/3)):
+			locations_panel[j][0] = (random.randint(0,num_locations-1), random.randint(0,num_locations-1))
+		locations_panel = np.array(locations_panel)
+		for j in range(int(num_spiders/3),num_spiders):
+			locations_panel[j][0] = random.choice(locations_panel[0:int(num_spiders/3),0])
+	else:
+		for j in range(int(num_spiders)):
+			locations_panel[j][0] = (random.randint(0,num_locations-1), random.randint(0,num_locations-1))
+		locations_panel = np.array(locations_panel)
 
 	#male_locations = [(random.randint(0,num_locations-1), random.randint(0,num_locations-1)) for i in range(numMales)]
 
@@ -169,18 +190,12 @@ def simulate(meta_parameters):
 			u = [[np.random.poisson(arrival_rate) for i in range(num_locations)] for k in range(num_locations)]
 		else: 
 			u = [[np.random.poisson(space_variant_arrival_rate_mat[i,k]) for i in range(num_locations)] for k in range(num_locations)]
-
-		#u = [[0 for i in range(num_locations)] for k in range(num_locations)]
-		#u[1][1] = 100
-		#print(t)
 		for j in range(num_spiders):
 			# If alive
 			if (locations_panel[j,t,0]>=0):
 				# Spiders eat. Update fitness and size at time t
-				F_panel[j,t] = fitness_func(locations_panel[j,t], j, F_panel, t, locations_panel[j,t],locations_panel[:,t], sizes_panel[:,t], u, num_spiders, xi, kappa)
+				F_panel[j,t] = fitness_func(locations_panel[j,t], j, t, locations_panel, sizes_panel[:,t], u, num_spiders, xi, kappa)
 				sizes_panel[j,t] = size_continuous(F_panel, j, t)
-				if (size_continuous_bool==False):
-					sizes_panel[:,t] = rescale_size(sizes_panel[:,t], num_spiders)
 			# If already dead
 			else:
 				if (t+1<T):
@@ -188,24 +203,27 @@ def simulate(meta_parameters):
 					sizes_panel[j,t] = 0
 					locations_panel[j,t+1, 0]=-99
 					locations_panel[j,t+1, 1]=-99
-		
-
-
+		if (t+1<T):
+			locations_panel[:,t+1] = locations_panel[:,t] 
 		for j in range(num_spiders):
-				if (t+1<T):
-					# After eating and updating size, if the the spider's size is less than 0, spider dies.
-					if (sizes_panel[j,t] < 0.0):
-						locations_panel[j,t+1, 0]=-99
-						locations_panel[j,t+1, 1]=-99
-					# If spider still alive, calculate fitness of potential moves and move to location providing highest expected fitness
-					else:
-						F_calc_lag[4] = F_calc_lag[3]
-						F_calc_lag[3] = F_calc_lag[2]
-						F_calc_lag[2] = F_calc_lag[1]
-						F_calc_lag[1] = F_calc_lag[0]
-						# Spiders calculate fitness from other locations
-						# Consider all locations
-						F_calc = [[fitness_func((i, k), j, F_panel, t, locations_panel[j,t],locations_panel[:,t], sizes_panel[:,t], u, num_spiders, xi, kappa) for i in range(num_locations)] for k in range(num_locations)]
+			# Rescale sizes after all spiders have eaten
+			if (size_continuous_bool==False):
+					sizes_panel[:,t] = rescale_size(sizes_panel[:,t], num_spiders)
+			if (t+1<T):
+				# After eating and updating size, if the the spider's size is less than 0, spider dies.
+				if (sizes_panel[j,t] < 0.0):
+					locations_panel[j,t+1, 0]=-99
+					locations_panel[j,t+1, 1]=-99
+				# If spider still alive, calculate fitness of potential moves and move to location providing highest expected fitness
+				else:
+					F_calc_lag[4] = F_calc_lag[3]
+					F_calc_lag[3] = F_calc_lag[2]
+					F_calc_lag[2] = F_calc_lag[1]
+					F_calc_lag[1] = F_calc_lag[0]
+					# Spiders calculate fitness from other locations
+					# Consider all locations
+					if (all_at_once==True):
+						F_calc = [[fitness_func((i, k), j, t, locations_panel, sizes_panel[:,t], u, num_spiders, xi, kappa) for i in range(num_locations)] for k in range(num_locations)]
 						F_calc = np.array(F_calc)
 						F_calc_lag[0] = F_calc
 						# Set out of range locations to -99 
@@ -224,16 +242,30 @@ def simulate(meta_parameters):
 						else:
 							F_calc_avg = np.array([[np.mean(F_calc_lag[:,i,j]) for i in range(num_locations)] for j in range(num_locations)])
 							k,l = np.unravel_index(np.argmax(F_calc_avg), F_calc_avg.shape)
-						# Unless at last time step, update locations_panel for where the spider moved
-						locations_panel[j,t+1, 0] = k
-						locations_panel[j,t+1, 1] = l
-					# if (j==1):
-					# 	print(F_calc)
-					# 	print(locations_panel[j,t+1])
-					# 	print(sizes_panel[j,t])
+					else:
+						F_calc = [[fitness_func((i, k), j, t+1, locations_panel, sizes_panel[:,t+1], u, num_spiders, xi, kappa) for i in range(num_locations)] for k in range(num_locations)]
+						F_calc = np.array(F_calc)
+						F_calc_lag[0] = F_calc
+						# Set out of range locations to -99 
+						if (omnicient==False):
+							for row in range(num_locations):
+								for col in range(num_locations):
+									if (row > (locations_panel[j,t+1, 0]+1) or row < (locations_panel[j,t+1, 0] -1) or col > (locations_panel[j,t+1, 1]+1) or col < (locations_panel[j,t+1, 1]-1)):
+										F_calc[row,col] = -99
+						if (random_movement == True):
+							for row in range(num_locations):
+								for col in range(num_locations):
+									F_calc[row,col] = F_calc[row,col] + np.random.normal(0, random_movement_sd)
+						# Save the coordinates of the location with highest expected fitness
+						if (smooth==False):
+							k,l = np.unravel_index(np.argmax(F_calc), F_calc.shape)
+						else:
+							F_calc_avg = np.array([[np.mean(F_calc_lag[:,i,j]) for i in range(num_locations)] for j in range(num_locations)])
+							k,l = np.unravel_index(np.argmax(F_calc_avg), F_calc_avg.shape)
 
-			
-
+					# Unless at last time step, update locations_panel for where the spider moved
+					locations_panel[j,t+1, 0] = k
+					locations_panel[j,t+1, 1] = l
 
 		# Count number of spiders at each location and save the count
 		for row in range(num_locations):
@@ -257,14 +289,15 @@ def simulate(meta_parameters):
 	with h5py.File('size.h5', 'w') as hf:
 		hf.create_dataset("sizes_panel",  data=sizes_panel)
 
+	with h5py.File('fitness.h5', 'w') as hf:
+		hf.create_dataset("F_panel",  data=F_panel)
+
 	with h5py.File('locations.h5', 'w') as hf:
 		hf.create_dataset("locations_panel",  data=locations_panel)
 
 	#print("Time to save data: " + str(time.time() - t))
 
-	print(number_at_location_time_series)
 	df1 = pd.DataFrame(sizes_panel)
-	print(sizes_panel)
 	#print(np.array(df1.agg(['mean'])))
 
 	df = pd.DataFrame(F_panel)
@@ -292,6 +325,10 @@ def graph(meta_parameters):
 	sizes_panel = list(sizes_panel["sizes_panel"])
 	sizes_panel = np.array(sizes_panel)
 
+	F_panel = h5py.File("fitness.h5", 'r')
+	F_panel = list(F_panel["F_panel"])
+	F_panel = np.array(F_panel)
+
 	number_at_location_time_series = h5py.File("locationNum.h5", 'r')
 	number_at_location_time_series = list(number_at_location_time_series["number_at_location_time_series"])
 	number_at_location_time_series = np.array(number_at_location_time_series)
@@ -300,7 +337,7 @@ def graph(meta_parameters):
 	locations_panel = list(locations_panel["locations_panel"])
 	locations_panel = np.array(locations_panel)
 
-	numLocations = len(locations_panel[1])
+	numLocations = locations_panel.shape[0]
 
 	def sdAtLoc(loc_y, loc_x, t):
 		sizes_at_location = np.zeros(len(locations_panel[:,0]))
@@ -321,14 +358,18 @@ def graph(meta_parameters):
 
 	t = time.time()
 	means = np.ones(len(sizes_panel[0,:]))
+	mean_fitness = np.ones(len(F_panel[0,:]))
 	intra_cluster_sds = np.ones(len(sizes_panel[0,:]))
 	num_dead = np.ones(len(sizes_panel[0,:]))
 	avg_num_at = np.ones(len(sizes_panel[0,:]))
+	num_clusters = np.zeros(len(sizes_panel[0,:]))
 	for t in range(len(sizes_panel[1])):
 		means[t] = np.mean(sizes_panel[(sizes_panel>0)[:,t],t])
+		mean_fitness[t] = np.mean(F_panel[(F_panel>0)[:,t],t])
 		num_dead[t] = len(sizes_panel[(sizes_panel<=0)[:,t],t])
 		intra_cluster_sds[t] = intracluster_sd(t)[0]
 		avg_num_at[t] = np.mean(number_at_location_time_series[t][np.nonzero(number_at_location_time_series[t])])
+		num_clusters[t] = np.count_nonzero(number_at_location_time_series[t])
 		#avg_num_at[t] = intracluster_sd(t)[1]
 
 
@@ -377,59 +418,70 @@ def graph(meta_parameters):
 	+ "timevar=" + boolString(meta_parameters['time_variant_arrival_rate'])
 	+ "spacevar=" + boolString(meta_parameters['space_variant_arrival_rate']) 
 	+ "omni=" + boolString(meta_parameters['omnicient']) + "rand=" + str(random_str) 
-	+ "smoo=" + boolString(meta_parameters['smooth']))
+	+ "smoo=" + boolString(meta_parameters['smooth']) + "once=" + boolString(meta_parameters['all_at_once']) + "sc=" + boolString(meta_parameters['start_clusters']))
 
 	plt.hist(nullSD)
 	plt.axvline(model, color='b', linestyle='dashed', linewidth=2)
 	plt.savefig('model_perm_test' + name_params + '.png', bbox_inches='tight')
 	plt.close()
 
+	plt.plot(mean_fitness)
+	plt.xlabel("Time")
+	plt.xlabel("Fitness")
+	plt.savefig('fitness_over_time' + name_params + '.png', bbox_inches='tight')
+	plt.close()
+
 	plt.plot(means)
+	plt.xlabel("Time")
+	plt.ylabel("Size")
 	plt.savefig('size_over_time' + name_params + '.png', bbox_inches='tight')
 	plt.close()
 
+	plt.plot(avg_num_at)
+	plt.xlabel("Time")
+	plt.ylabel("Number per Cluster")
+	plt.savefig('num_at_over_time' + name_params + '.png', bbox_inches='tight')
+	plt.close()
+
+	plt.plot(num_clusters)
+	plt.xlabel("Time")
+	plt.ylabel("Number of Clusters")
+	plt.savefig('num_clusters' + name_params + '.png', bbox_inches='tight')
+	plt.close()
+
 	plt.plot(intra_cluster_sds)
+	plt.xlabel("Time")
+	plt.ylabel("Intra-cluster Size SD")
 	plt.savefig('intracluster_sd_over_time' + name_params + '.png', bbox_inches='tight')
 	plt.close()
 
-	plt.imshow(number_at_location_time_series[0], cmap='hot', interpolation='nearest')
-	plt.savefig('heatmap_t=0' + name_params + '.png', bbox_inches='tight')
-	plt.close()
 
-	plt.imshow(number_at_location_time_series[25], cmap='hot', interpolation='nearest')
-	plt.savefig('heatmap_t=25' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-
-	plt.imshow(number_at_location_time_series[50], cmap='hot', interpolation='nearest')
-	plt.savefig('heatmap_t=50' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-
-	plt.imshow(number_at_location_time_series[75], cmap='hot', interpolation='nearest')
-	plt.savefig('heatmap_t=75' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-
-	t = len(sizes_panel[1])-1
-	plt.imshow(number_at_location_time_series[t], cmap='hot', interpolation='nearest')
-	plt.savefig('heatmap_t=' + str(t) + name_params + '.png', bbox_inches='tight')
+	im = plt.imshow(number_at_location_time_series[len(sizes_panel[1])-1], interpolation='nearest', cmap = 'hot', extent=[0,numLocations,0,numLocations])
+	clim=im.properties()['clim']
 	plt.close()
 
 
-	plt.hist(sizes_panel[:,0])
-	plt.savefig('hist_t=0' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-	plt.hist(sizes_panel[:,25])
-	plt.savefig('hist_t=25' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-	plt.hist(sizes_panel[:,50])
-	plt.savefig('hist_t=50' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-	plt.hist(sizes_panel[:,75])
-	plt.savefig('hist_t=75' + name_params + '.png', bbox_inches='tight')
-	plt.close()
-	plt.hist(sizes_panel[:,99])
-	plt.savefig('hist_t=99' + name_params + '.png', bbox_inches='tight')
-	plt.close()
+	# for i in np.linspace(0, len(sizes_panel[1])-1, len(sizes_panel[1]-1)/24):
+	# 	fig = plt.figure()
+	# 	grid = axes_grid1.AxesGrid(fig, 111, nrows_ncols=(1, 1), axes_pad = 0.5, cbar_location = "right", 
+	# 		cbar_mode="each", cbar_size="15%", cbar_pad="5%")
+	# 	im = grid[0].imshow(number_at_location_time_series[int(i)], interpolation='nearest', cmap = 'hot', clim = clim, extent=[0,numLocations,0,numLocations])
+	# 	grid.cbar_axes[0].colorbar(im)
+	# 	plt.savefig('heatmap_t=' + str(int(i)) + name_params + '.png', bbox_inches='tight')
+	# 	plt.close(fig)
+	# 	plt.hist(sizes_panel[:,int(i)], range=(0,np.amax(sizes_panel)))
+	# 	plt.savefig('hist_t=' + str(int(i)) + name_params + '.png', bbox_inches='tight')
+	# 	plt.close()
 
+	for i in range(0, len(sizes_panel[1])-1):
+		fig = plt.figure()
+		grid = axes_grid1.AxesGrid(fig, 111, nrows_ncols=(1, 1), axes_pad = 0.5, cbar_location = "right", 
+			cbar_mode="each", cbar_size="15%", cbar_pad="5%")
+		im = grid[0].imshow(number_at_location_time_series[int(i)], interpolation='nearest', cmap = 'hot', clim = clim, extent=[0,numLocations,0,numLocations])
+		grid.cbar_axes[0].colorbar(im)
+		fig.suptitle('t = ' + str(int(i)))
+		plt.savefig('heatmap_' + name_params + 't=' + str(int(i)).zfill(3) + '.png', bbox_inches='tight')
+		plt.close(fig)
 
 
 # num_spiders: Number of (female) spiders to begin with
